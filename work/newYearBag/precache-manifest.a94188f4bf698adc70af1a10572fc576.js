4ce3b320fda551f4d2a42fe1e7db8507fc38da79
licong96 <licong19961204@gmail.com>
2020-10-11 10:48:26 +0800

    更新两个动画项目

---
+++ work/newYearBag/precache-manifest.a94188f4bf698adc70af1a10572fc576.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "62fb1528df750d9763e5",
+    "url": "./static/css/index.7945c680.chunk.css"
+  },
+  {
+    "revision": "62fb1528df750d9763e5",
+    "url": "./static/js/index.62fb1528.chunk.js"
+  },
+  {
+    "revision": "1ba06b90ddce28b5f57b",
+    "url": "./static/js/1.1ba06b90.chunk.js"
+  },
+  {
+    "revision": "42ed6b1d22a090a16a8e",
+    "url": "./static/js/runtime~index.42ed6b1d.js"
+  },
+  {
+    "revision": "57ba97c0d0267f1ada9f9a8b96376073",
+    "url": "./index.html"
+  }
+];
