d1fd25c012f7091688397cf12607c8600ffab680
licong96 <licong19961204@gmail.com>
2020-10-31 17:46:33 +0800

    添加语言项目

---
+++ language/precache-manifest.8993fa87c031996d467245bfc28dba46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db9a067696db567edcc8d9592bb618fc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e7a30be3f7ada2454e21",
+    "url": "./static/css/main.52cd5a3c.chunk.css"
+  },
+  {
+    "revision": "21ef7408077b28d289f2",
+    "url": "./static/js/2.fd323a1a.chunk.js"
+  },
+  {
+    "revision": "e7a30be3f7ada2454e21",
+    "url": "./static/js/main.0e023e10.chunk.js"
+  },
+  {
+    "revision": "d32905f55bf3fea23eef",
+    "url": "./static/js/runtime~main.a4123ff8.js"
+  }
+]);
